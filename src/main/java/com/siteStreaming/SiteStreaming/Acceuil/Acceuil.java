1648eb32cf8c6c5bb1ae6f011214464dfba06bb9
jhSaleh <jsaleh@enssat.fr>
2021-03-07 16:30:24 +0100

    -déplacement de index.jsp dans le web-inf -ajout d'image sur la page d'acceuil

--- src/main/java/com/siteStreaming/SiteStreaming/Acceuil/Acceuil.java
+++ src/main/java/com/siteStreaming/SiteStreaming/Acceuil/Acceuil.java
@@ -1,6 +1,8 @@
 package com.siteStreaming.SiteStreaming.Acceuil;
 
+import com.siteStreaming.SiteStreaming.Access.AdminFilter;
 import com.siteStreaming.SiteStreaming.Access.ConnectedUserFilter;
+import com.siteStreaming.SiteStreaming.DataBase.AdministratorDatabase;
 import com.siteStreaming.SiteStreaming.DataBase.ClientDatabase;
 
 import javax.servlet.RequestDispatcher;
@@ -19,31 +21,47 @@ public class Acceuil extends HttpServlet {
         String mail = request.getParameter("mailAddress");
         String password = request.getParameter("password");
 
-        if(mail != null){ //Si un formulaire a été envoyé
+        if(mail != null) { //Si un formulaire a été envoyé
             System.out.println("Connection en cours. Preparation servlet.");
             ClientDatabase clientDatabase = new ClientDatabase();
             String infoClient[] = clientDatabase.getAllClientInformation(mail);
             CompteClient compteClient = clientDatabase.getCompteClient(mail);
 
-            if(compteClient != null){
-                System.out.println("Mdp entrée   : "+password);
-                System.out.println("Mdp comparé  : "+compteClient.getPassword());
-                if(compteClient.isPassWord(password)){ //Vérification du mdp
+            AdministratorDatabase administratorDatabase = new AdministratorDatabase();
+            CompteAdmin compteAdmin = administratorDatabase.getCompteAdmin(mail);
+
+            if (compteClient != null) {
+                System.out.println("Mdp entrée   : " + password);
+                System.out.println("Mdp comparé  : " + compteClient.getPassword());
+                if (compteClient.isPassWord(password)) { //Vérification du mdp
 
                     //Essaie
                     System.out.println("Coté serveur : Connection réussit.");
                     HttpSession session = request.getSession(); //Création d'une session utilisateur s'il n'a pas été créé avant
                     session.setAttribute(ConnectedUserFilter.sessionUtilisateur, compteClient); //Ajoute à la session la notion de client
 
-
+                    /*
                     request.setAttribute("infoClient", infoClient); //transmet l'info à d'autre servlet et la page html
                     request.setAttribute("signedInSent", true);
+                     */
                 } else {
                     System.out.println("Coté serveur : Echec de connection renvoit des données.");
                     request.setAttribute("mailAddressUsed", mail);
                     request.setAttribute("passwordUsed", password);
                 }
-            } else {
+            }else if(compteAdmin != null){
+                if(compteAdmin.isPassWord(password)){
+                    System.out.println("Administrateur trouvé !");
+                    HttpSession session = request.getSession(); //Création d'une session utilisateur s'il n'a pas été créé avant
+                    session.setAttribute(AdminFilter.sessionAdmin, compteAdmin); //Création de la session administrateur
+
+                    //Ajouté la redirection vers la bonne page admin
+                } else {
+                    System.out.println("Coté serveur : Echec de connection renvoit des données.");
+                    request.setAttribute("mailAddressUsed", mail);
+                    request.setAttribute("passwordUsed", password);
+                }
+            }else {
                 System.out.println("Coté serveur : Echec de connection renvoit des données.");
                 request.setAttribute("mailAddressUsed", mail);
                 request.setAttribute("passwordUsed", password);
@@ -54,7 +72,7 @@ public class Acceuil extends HttpServlet {
 
 
         //Redirige vers la page d'acceuil
-        String pageName = "/index.jsp";
+        String pageName = "/WEB-INF/index.jsp";
         RequestDispatcher rd = getServletContext().getRequestDispatcher(pageName);
 
         try {
---
+++ src/main/java/com/siteStreaming/SiteStreaming/DataBase/AdministratorDatabase.java
@@ -0,0 +1,171 @@
+package com.siteStreaming.SiteStreaming.DataBase;
+import com.siteStreaming.SiteStreaming.Acceuil.CompteAdmin;
+import com.siteStreaming.SiteStreaming.Acceuil.CompteClient;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class AdministratorDatabase {
+    public Connection connection;
+    public Statement statement;
+
+
+    /**
+     * Crée une connection avec la base de données
+     */
+    public AdministratorDatabase(){
+        try{
+            this.connection = DBManager.getInstance().getConnection();
+            this.statement = this.connection.createStatement();
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+
+
+
+    /**
+     * Vérifie si un client est présent dans une bdd
+     * @param mail
+     * @return
+     */
+    public boolean isAdminInDatabase(String mail){
+        try{
+            mail = S.cd(mail);
+            String query = "SELECT * FROM CompteClient where adresseMailClient="+mail+";";
+            ResultSet res = this.statement.executeQuery(query);
+            if(res.next()){ //S'il y a un élément dans le résultat, c'est que le client est présent dans la bdd
+                return true;
+            } else {
+                return false;
+            }
+        } catch(SQLException e){
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    /**
+     * Renvoit le nom et prénom d'un client
+     * @param mail
+     * @return
+     */
+    public String[] getFirstNameLastName(String mail){
+        try {
+            mail = "'"+mail+"'";
+
+            String query = "SELECT * FROM Compte where adresseMail ="+mail+";";
+            ResultSet res = this.statement.executeQuery(query);
+            if (res.next()) { //S'il y a un élément dans le résultat, c'est que le client est présent dans la bdd
+                String nom = res.getString("nom");
+                String prenom = res.getString("prenom");
+                String[] answer = {nom, prenom};
+                return answer;
+            } else {
+                return null;
+            }
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public String[] getAllClientInformation(String mail){
+        try {
+            mail = "'"+mail+"'";
+
+            String query = "SELECT * FROM Compte where adresseMail ="+mail+";";
+            ResultSet res = this.statement.executeQuery(query);
+            if (res.next()) { //S'il y a un élément dans le résultat, c'est que le client est présent dans la bdd
+                String[] answer = {res.getString("adresseMail"), res.getString("civilite"), res.getString("nom"), res.getString("prenom"), res.getString("motDePasse"), res.getString("dateNaissance")};
+                //System.out.println("Tte les infos clients : "+ answer[0] +answer[1] + answer[3] + answer[4] + " etc.");
+                return answer;
+            } else {
+                return null;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * Renvoit le compte client s'il existe pour un mail donné
+     * @param mail
+     * @return
+     */
+    public CompteAdmin getCompteAdmin(String mail){
+        String nom, prenom, civilite, email, mdp, dateNaissance, isProfilManagerClient;
+        try{
+            mail = "'"+mail+"'";
+
+            String query = "SELECT * FROM Compte where adresseMail ="+mail+";";
+            ResultSet res = this.statement.executeQuery(query);
+            if (res.next()) { //S'il y a un élément dans le résultat, c'est que le client est présent dans la bdd
+
+                //Récupération des élèments de compte client
+                nom = res.getString("nom");
+                prenom = res.getString("prenom");
+                civilite = res.getString("civilite");
+                email = res.getString("adresseMail");
+                mdp = res.getString("motDePasse");
+                dateNaissance = res.getString("dateNaissance");
+
+                String query2 = "SELECT * FROM CompteAdmin where adresseMailAdmin ="+mail+";";
+                ResultSet res2 = this.statement.executeQuery(query2);
+                if (res2.next()){
+                    isProfilManagerClient = res2.getString("isProfilManagerClient");
+                    System.out.println("Succes finding the admin !");
+                    return new CompteAdmin(nom, prenom, civilite, email, mdp, dateNaissance, isProfilManagerClient);
+                }
+
+                return null;
+            } else {
+                return null;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * Ajoute un client à la base de données
+     * Attention, cela ne vérifie pas s'il y a respect de la clé primaire, i.e. un compte = un mail
+     * @param compte
+     * @return boolean indiquant le succès de l'opération
+     */
+    public boolean addAdminAccount(CompteAdmin compte){
+        try {
+            String addToCompte = "insert into `Compte` values("+compte.getMailD()+","+
+                    compte.getCiviliteD()+","+
+                    compte.getNomD()+","+
+                    compte.getPrenomD()+","+
+                    compte.getPasswordD()+","+
+                    compte.getBirthDateD()+
+                    ");";
+            System.out.println(addToCompte);
+            this.statement.executeUpdate(addToCompte);
+
+            this.statement.executeUpdate("insert into `CompteAdmin` values(NULL,"+compte.getMailD()+","+compte.getIsProfilManagerClientD()+");");
+            System.out.println("Admin ajouté à la bdd!");
+            return true;
+        } catch (SQLException e){
+            System.out.println("Admin non ajouté à la bdd!");
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public static void main(String[] args){
+        //Tests
+        AdministratorDatabase administratorDatabase = new AdministratorDatabase();
+        CompteAdmin compteAdmin = new CompteAdmin("Josh", "ManageClient", "Monsieur", "joshS@gmail.com", "fjYn7p6yBcwWaPp", "1988-12-02", "true");
+        administratorDatabase.addAdminAccount(compteAdmin);
+        CompteAdmin compteAdmin2 = new CompteAdmin("Tyler", "ManageMusicLibrary", "Monsieur", "joshS2@gmail.com", "fjYn7p6yBcwWaPp", "1988-12-02", "false");
+        administratorDatabase.addAdminAccount(compteAdmin2);
+    }
+
+}
