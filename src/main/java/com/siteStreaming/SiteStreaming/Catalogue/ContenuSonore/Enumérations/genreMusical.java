d887ca96acecd09f3227a5b8bb656b27f5b59959
JuiletteD <72203353+JuiletteD@users.noreply.github.com>
2021-03-13 11:12:34 +0100

    Update genreMusical.java

--- src/main/java/com/siteStreaming/SiteStreaming/Catalogue/ContenuSonore/Enumérations/genreMusical.java
+++ src/main/java/com/siteStreaming/SiteStreaming/Catalogue/ContenuSonore/Enumérations/genreMusical.java
@@ -4,4 +4,4 @@ package com.siteStreaming.SiteStreaming.Catalogue.ContenuSonore.Enumérations;
  * Genre musicaux des musiques ou des radios
  */
 public enum genreMusical {
-blues, country, disco, gospel, hard_rock,jazz, metal, pop, punk, rap, rock_n_roll, salsa, classique, baroque, romantique, médiévale,house  }
+blues, country, disco, gospel, hard_rock,jazz, metal, pop, punk, rap, rock_n_roll, salsa, classique, baroque, romantique, médiévale,house,métal  }
